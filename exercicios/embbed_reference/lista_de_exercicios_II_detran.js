683038e3641644b71373563a0528aa39a10b4db8
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2026-05-13 12:32:42 -0300

    Adicionar Aula 11 e 12 - pdf

---
+++ exercicios/embbed_reference/lista_de_exercicios_II_detran.js
@@ -0,0 +1,33 @@
+// 1) Qual modelo de carro tem mais multas?
+db.veiculos.aggregate([
+    {
+        
+    }
+]);
+
+// 2) Quantas multas por cidade? 
+
+
+// 3) Qual é a infração mais aplicada? 
+
+
+// 4) Qual mês do ano tem mais multas? 
+
+
+// 5) Qual é a cor de veículo mais multada? 
+
+
+// 6) Qual agente aplica mais multas? 
+
+
+// 7) Qual sexo é mais multado? 
+
+
+// 8) Qual marca de carro os homens preferem? 
+
+
+// 9) Qual cor de carro as mulheres mais preferem? 
+
+
+// 10) Faça um ranking dos veículos mais multados, decrescente.
+
